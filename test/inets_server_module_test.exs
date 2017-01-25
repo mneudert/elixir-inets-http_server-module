3b4c58fcf4720f702e9017a3abb6279b8d14a417
Marc Neudert <marc.neudert@gmail.com>
2017-01-25 19:31:41 +0100

    configures httpd_server for tests

--- test/inets_server_module_test.exs
+++ test/inets_server_module_test.exs
@@ -1,8 +1,13 @@
 defmodule InetsServerModuleTest do
   use ExUnit.Case
-  doctest InetsServerModule
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "httpd server responding" do
+    port     = Application.get_env(:inets_server_module, :port)
+    response =
+      "http://localhost:#{ port }/index.html"
+      |> to_char_list()
+      |> :httpc.request()
+
+    assert { :ok, {{ _, 200, _ }, _, 'document root\n' }} = response
   end
 end
--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,14 @@
+root          = Path.join([ __DIR__, "document_root" ]) |> to_char_list()
+httpd_config  = [
+  document_root: root,
+  port:          0,
+  server_name:   'elixir_httpd_module_test',
+  server_root:   root
+]
+
+:ok                = :inets.start()
+{ :ok, httpd_pid } = :inets.start(:httpd, httpd_config)
+
+Application.put_env(:inets_server_module, :port, :httpd.info(httpd_pid)[:port])
+
 ExUnit.start()

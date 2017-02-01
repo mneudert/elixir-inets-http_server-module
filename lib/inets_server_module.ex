4ca01b32fc65abd18025fd1b0c5b3234d55c916c
Marc Neudert <marc.neudert@gmail.com>
2017-02-01 19:29:56 +0100

    defines sleeping url

--- lib/inets_server_module.ex
+++ lib/inets_server_module.ex
@@ -5,14 +5,30 @@ defmodule InetsServerModule do
 
   Record.defrecord :mod, Record.extract(:mod, from_lib: "inets/include/httpd.hrl")
 
-  def serve(mod_data) do
-    body = 'document: ' ++ mod(mod_data, :request_uri)
-    head = [
+  def serve(mod_data), do: serve_uri(mod(mod_data, :request_uri), mod_data)
+
+
+  defp headers(body) do
+    [
       code:           200,
       content_length: body |> length() |> to_charlist(),
       content_type:   'text/html'
     ]
+  end
+
+  defp serve_uri('/index.html', _mod_data) do
+    body = 'document: /index.html'
+    head = headers(body)
 
     {:proceed, [{:response, {:response, head, body}}]}
+  end
+
+  defp serve_uri('/sleepy.head/500', _mod_data) do
+    :timer.sleep(500)
+
+    body = 'slept for 500'
+    head = headers(body)
+
+    { :proceed, [{ :response, { :response, head, body }}]}
   end
 end
--- test/inets_server_module_test.exs
+++ test/inets_server_module_test.exs
@@ -10,4 +10,16 @@ defmodule InetsServerModuleTest do
 
     assert { :ok, {{ _, 200, _ }, _, 'document: /index.html' }} = response
   end
+
+  test "httpd server sleeping response" do
+    port               = Application.get_env(:inets_server_module, :port)
+    { time, response } = :timer.tc fn ->
+      "http://localhost:#{ port }/sleepy.head/500"
+      |> to_charlist()
+      |> :httpc.request()
+    end
+
+    assert { :ok, {{ _, 200, _ }, _, 'slept for 500' }} = response
+    assert time >= 500
+  end
 end
